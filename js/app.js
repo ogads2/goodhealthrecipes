c73586b0ecc6cc718b41003318305763dfe16f1e
ogads2 <165876124+ogads2@users.noreply.github.com>
2024-04-26 12:57:30 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 function og_locker(){
-    // window.location.href='https://verifyuser.org/cl/i/8d95ld';
-    window.alert("hello");
+    window.location.href='https://verifyuser.org/cl/i/8d95ld';
+    // window.alert("hello");
 }
